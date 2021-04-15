699d0a9a9e1365d4900209edb5e064c5ec5a6c17
jmmemo <18320320986@163.com>
2021-04-15 11:52:59 +0800

    edit in kiri branch ,push to main

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("main test Git")
+	fmt.Println("fourth commit by jmmemo  change in kiri branch,push to main branch")
 	//增加文件,然后 second commit
+	//修改文件 ,fourth commit
+
 }
